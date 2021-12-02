13d63d14932caadae85bb5e7a9f8498854e1bac8
Pierre Leresteux <pierre.leresteux@gmail.com>
2021-12-02 12:29:59 +0100

    :sparkles: Day2 - Add alternatives using `fold` instead of `forEach`

--- src/main/kotlin/me/pierre/aoc2021/Day2.kt
+++ src/main/kotlin/me/pierre/aoc2021/Day2.kt
@@ -6,7 +6,9 @@ class Day2 {
     fun run() {
         val sonarCourse = File("src/main/resources/day2/input.txt").readLines().map(String::toCommand)
         step1(sonarCourse)
+        step1Alt(sonarCourse)
         step2(sonarCourse)
+        step2Alt(sonarCourse)
     }
 
     private fun step1(sonarCourse: List<Command>) {
@@ -20,6 +22,18 @@ class Day2 {
             }
         }
         println(" Step 1 : ${horizontal * depth}")
+    }
+
+    private fun step1Alt(sonarCourse: List<Command>) {
+        val (h, d) = sonarCourse.fold(Step1Fold(0, 0)) { acc, command ->
+            when (command.instruction) {
+                "forward" -> acc.horizontal += command.value
+                "down" -> acc.depth += command.value
+                "up" -> acc.depth -= command.value
+            }
+            acc
+        }
+        println(" Step 1 Alt : ${h * d}")
     }
 
     private fun step2(sonarCourse: List<Command>) {
@@ -38,6 +52,21 @@ class Day2 {
         }
         println(" Step 2 : ${horizontal * depth}")
     }
+
+    private fun step2Alt(sonarCourse: List<Command>) {
+        val (h, d) = sonarCourse.fold(Step2Fold(0, 0, 0)) { acc, command ->
+            when (command.instruction) {
+                "forward" -> {
+                    acc.horizontal += command.value
+                    acc.depth += acc.aim * command.value
+                }
+                "down" -> acc.aim += command.value
+                "up" -> acc.aim -= command.value
+            }
+            acc
+        }
+        println(" Step 2 Alt : ${h * d}")
+    }
 }
 
 fun String.toAction() = this.filter { it.isLetter() }
@@ -46,6 +75,9 @@ fun String.toIntValue() = this.filter { it.isDigit() }
 data class Command(
     val instruction: String, val value: Int
 )
+
+data class Step1Fold(var horizontal: Int, var depth: Int)
+data class Step2Fold(var horizontal: Int, var depth: Int, var aim: Int)
 
 fun String.toCommand() = Command(this.toAction(), this.toIntValue().toInt())
 
